fca128c3ca876c68c815875c7e8dfb17b57d83dc
ymataku <s256282@wakayama-u.ac.jp>
2022-08-28 17:18:23 +0900

    make it better

--- myproject/views.py
+++ myproject/views.py
@@ -1,9 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import User
+from .models import Test
+from django import forms 
+# from models import User
 
 def index(request):
-    # 変数設定
+    sample_list = Test.objects.all()
     params = {"message_me": "Hello World"}
+    # form = SampleForm(request.POST or None)
+    context = {
+        'sample_list':sample_list,
+        'params':params,
+    }
+    # 変数設定
+    
     # 出力
-    return render(request,'App_Folder_HTML/index.html',context=params)
+    if request.method == 'POST':
+        user_name = is_private = request.POST.get('name', False)
+        user_id = is_private = request.POST.get('id', False)
+        Test.objects.create(
+            test_name=user_name,
+            tesetr_id=user_id,
+        )
+            # create()の場合
+            # sample.objects.create(**form.cleaned_data)
+
+            # save()の場合
+            # sample = Sample(**form.cleaned_data)
+            # sample.save
+    return render(request,'App_Folder_HTML/index.html',context=context)
+
